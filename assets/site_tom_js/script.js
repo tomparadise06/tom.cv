bfc57a36237ca60cae6f03e008ef6126eb059d07
Tom Mergui <115085780+Paradise2880@users.noreply.github.com>
2023-04-10 19:02:50 +0200

    update new change

--- assets/site_tom_js/script.js
+++ assets/site_tom_js/script.js
@@ -1,33 +1,87 @@
-
-const profileImage = document.querySelector('.profile-picture img');
-
-
-profileImage.addEventListener('mouseover', () => {
-  profileImage.style.transform = 'scale(1.2)';
+Vue.component('portfolio-item',{
+    template:`
+        <div class="portfolio-item" @click="redirect">
+            <div class="pi-info">
+                <h3 class="pi-title">{{ pi_title }}</h3>
+                <p class="pi-desc">{{ pi_desc }}</p>
+            </div>
+        </div>
+    `,
+    props: ["pi_title","pi_desc"],
+    methods: {
+        redirect: function () {
+            if (this.pi_title == 'Les Vieilles Charettes') {
+                window.open('http://oc7prawfzz.preview.infomaniak.website/', '_blank');
+            }
+            else if (this.pi_title == 'Maison Levasseur') {
+                window.open('https://www.figma.com/file/40bPtFzbyO7tmnP6Vx9sdh/Pr%C3%A9sentation-Maison-Levasseur?node-id=0%3A1&t=gARiwMHpExicFMnT-1', '_blank');
+            }
+            else if (this.pi_title == 'Instant Gaming') {
+                window.open('https://hawksleynem.github.io/instant-gaming/', '_blank');
+            }
+            else if (this.pi_title == 'Musée du Petit Palais') {
+                window.open('https://www.figma.com/file/pNL1jkP3oRBQmSvZVAywgq/Mus%C3%A9e-du-Petit-Palais?node-id=0%3A1&t=z1E3bCW1p5vviBlx-1', '_blank');
+            }
+            else if (this.pi_title == 'Popchill') {
+                window.open('https://hawksleynem.github.io/popchill/', '_blank');
+            }
+            else if (this.pi_title == 'Les Trouvailles de Thaïs') {
+                window.open('https://les-trouvailles-de-thais.les-vielles-charrettes-tm.fr/', '_blank');
+            }
+            else if (this.pi_title == 'FluXML') {
+                window.open('https://hector-deal.alwaysdata.net/XML-Projet/', '_blank')
+            }
+        },
+    },
 });
 
-profileImage.addEventListener('mouseout', () => {
-  profileImage.style.transform = 'scale(1)';
+Vue.component('section-title',{
+    template:`
+        <div class="section-upper">
+            <div class="section-title-deco">
+                <hr class="section-title-hr-left hr-small" />
+                <hr class="section-title-hr-left" />
+                <hr class="section-title-hr-left" />
+                <hr class="section-title-hr-left" />
+                <hr class="section-title-hr-left" />
+                <div class="section-title-square"></div>
+                <h2 class="section-title">{{ title }}</h2>
+                <div class="section-title-square"></div>
+                <hr class="section-title-hr-right" />
+                <hr class="section-title-hr-right" />
+                <hr class="section-title-hr-right" />
+                <hr class="section-title-hr-right" />
+                <hr class="section-title-hr-right hr-small" />
+            </div>
+            <div>
+                <p class="section-desc">{{ desc }}</p>
+            </div>
+        </div>
+    `,
+    props: ["title","desc"],
 });
 
-// navLinks.forEach((link) => {
-//   link.addEventListener('click', (event) => {
-//     event.preventDefault();
-//     const target = event.target.getAttribute('href');
-//     const section = document.querySelector(target);
-//     const yOffset = -80;
-//     const y = section.getBoundingClientRect().top + window.pageYOffset + yOffset;
-//     window.scrollTo({top: y, behavior: 'smooth'});
-//   });
-// });
+Vue.component('si-title',{
+    template:`
+        <div class="si-title-deco">
+            <div class="section-title-square"></div>
+            <h3>{{ title }}</h3>
+            <div class="section-title-square"></div>
+        </div>
+    `,
+    props: ["title"],
+});
+
+var nv = new Vue ({
+    el:'#app'
+})
 
 
-// Charger la bibliothèque JavaScript de Font Awesome
-window.onload = function() {
-  var script = document.createElement("script");
-  script.src = "https://kit.fontawesome.com/xxxxxxxxxx.js"; // Remplacez "xxxxxxxxxx" par votre clé API Font Awesome
-  script.type = "text/javascript";
-  document.head.appendChild(script);
-}
+var win = window,
+docEl = document.documentElement,
+$nav = document.querySelector('nav');
 
-
+win.onscroll = function(){
+    var sTop = (this.pageYOffset || docEl.scrollTop)  - (docEl.clientTop || 0);
+    $nav.style.backgroundColor =  sTop > 500 ? "rgba(0, 0, 0, 1)":"rgba(0, 0, 0, 0)" ;
+ };
